d67af98a228a14dcac1edbbe4e65caa6afdb0183
Mehmet Celal <mehmet.celal.kara@macellan.net>
2022-02-02 14:40:38 +0300

    express install

--- socket.js
+++ socket.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+})
+app.listen(port);
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
